8962d7db3b65469f65dc0815aab1cfbfb6235571
soohwan hyun <xjavalov@shhyun.com>
2019-12-24 12:48:11 +0900

    something strange delete.

--- checker_delete.py
+++ checker_delete.py
@@ -16,17 +16,20 @@ class Application(tk.Frame):
         self.master = master
         self.pack()
 
-        self.dir = '/home/shhyun/drv_a/temporary/'
-        self.jpeg_dir = self.dir + 'JPEGImages/'
-        self.label_dir = self.dir + 'Annotations/'
+        self.dir = '/home/shhyun/raid/images/'
+        self.jpeg_dir = self.dir
 
-        self.target_dir = '/home/shhyun/drv_a/PotholeTestSet_20191223/VOCdevkit/VOC2012/'
+        self.target_dir = '/home/shhyun/drv_a/PotholeSet_20191210/VOCdevkit/VOC2012/'
         self.target_jpeg_dir = self.target_dir + 'JPEGImages/'
         self.target_label_dir = self.target_dir + 'Annotations/'
 
+        self.test_target_dir = '/home/shhyun/drv_a/PotholeTestSet_20191223/VOCdevkit/VOC2012/'
+        self.test_target_jpeg_dir = self.test_target_dir + 'JPEGImages/'
+        self.test_target_label_dir = self.test_target_dir + 'Annotations/'
+
         self.filelist = []
         self.label_idx = 1
-        self.current_idx = 0
+        self.current_idx = 8000
 
         for (path, dir, files) in os.walk(self.jpeg_dir):
             for filename in files:
@@ -38,6 +41,7 @@ class Application(tk.Frame):
 
         self.create_widgets()
         self.bind('<s>', self.save_cmd)
+        self.bind('<d>', self.delete_cmd)
         self.bind('<n>', self.next_cmd)
         self.bind('<p>', self.prev_cmd)
 
@@ -47,28 +51,36 @@ class Application(tk.Frame):
         self.save["command"] = self.save_cmd
         self.save.grid(row=0, column=0)
 
+        self.delete = tk.Button(self)
+        self.delete["text"] = "Delete"
+        self.delete["command"] = self.delete_cmd
+        self.delete.grid(row=0, column=1)
+
         self.next = tk.Button(self)
         self.next["text"] = "Next"
         self.next["command"] = self.next_cmd
-        self.next.grid(row=0, column=1)
+        self.next.grid(row=0, column=2)
 
         self.prev = tk.Button(self)
         self.prev["text"] = "Prev"
         self.prev["command"] = self.prev_cmd
-        self.prev.grid(row=0, column=2)
+        self.prev.grid(row=0, column=3)
 
         self.img1 = Image.open(self.filelist[self.current_idx])
         print(self.filelist[self.current_idx], self.img1)
+        self.img1 = self.img1.resize((1024, 768), Image.ANTIALIAS)
         self.render1 = ImageTk.PhotoImage(self.img1)
         self.image1 = Label(self, image=self.render1)
         self.image1.grid(row=1, column=0)
 
-        #self.img2 = Image.open("test.png")
-        #self.render2 = ImageTk.PhotoImage(self.img2)
-        #self.image2 = Label(self, image=self.render2)
-        #self.image2.grid(row=1, column=2)
-
         filename = self.filelist[self.current_idx].split('/')[-1][:-4]
+        self.img2 = Image.open(self.target_jpeg_dir + filename + '.jpg')
+        self.img2 = self.img2.resize((1024, 768), Image.ANTIALIAS)
+        self.render2 = ImageTk.PhotoImage(self.img2)
+        self.image2 = Label(self, image=self.render2)
+        self.image2.grid(row=1, column=2)
+
+
         self.label = Label(self, text="{0:05d}".format(self.current_idx) + "/" + "{0:05d}".format(len(self.filelist)) +
                                       "/" + filename)
         self.label.grid(row=2, column=0)
@@ -77,6 +89,26 @@ class Application(tk.Frame):
         self.quit.grid(row=0, column=3)
 
     def save_cmd(self, event=None):
+        # self.img.save(self.label_dir + "{0:05d}".format(self.label_idx) + "_" + "{0:05d}".format(self.current_idx) + ".jpg")
+        # shutil.copy(self.filelist[self.current_idx], self.target_dir + filename)
+
+        # filename = self.label_dir + "{0:05d}".format(self.label_idx) + "_" + "{0:05d}".format(self.current_idx) + "_" \
+        #           + datetime.today().strftime('%Y-%m-%d') + ".jpg"
+
+        filename = self.filelist[self.current_idx].split('/')[-1][:-4]
+        print(os.path.isfile(self.target_jpeg_dir + filename + ".jpg") and os.path.isfile(
+            self.target_label_dir + filename + ".xml"))
+        if os.path.isfile(self.target_jpeg_dir + filename + ".jpg") and os.path.isfile(self.target_label_dir + filename + ".xml"):
+            shutil.copy(self.target_jpeg_dir + filename + ".jpg", self.test_target_jpeg_dir + filename + ".jpg")
+            shutil.copy(self.target_label_dir + filename + ".xml", self.test_target_label_dir + filename + ".xml")
+            print(self.target_jpeg_dir + filename + ".jpg", self.target_label_dir + filename + ".xml")
+
+        self.label_idx += 1
+        self.current_idx += 1
+        self.update_display()
+        self.update_label()
+
+    def delete_cmd(self, event=None):
         #self.img.save(self.label_dir + "{0:05d}".format(self.label_idx) + "_" + "{0:05d}".format(self.current_idx) + ".jpg")
         #shutil.copy(self.filelist[self.current_idx], self.target_dir + filename)
 
@@ -84,11 +116,14 @@ class Application(tk.Frame):
         #           + datetime.today().strftime('%Y-%m-%d') + ".jpg"
 
         filename = self.filelist[self.current_idx].split('/')[-1][:-4]
-        print(os.path.isfile(self.jpeg_dir + filename + ".jpg") and os.path.isfile(self.label_dir + filename + ".xml"))
-        if os.path.isfile(self.jpeg_dir + filename + ".jpg") and os.path.isfile(self.label_dir + filename + ".xml"):
-            shutil.copy(self.jpeg_dir + filename + ".jpg", self.target_jpeg_dir + filename + ".jpg")
-            shutil.copy(self.label_dir + filename + ".xml", self.target_label_dir + filename + ".xml")
-            print(self.target_jpeg_dir + filename + ".jpg", self.target_label_dir + filename + ".xml")
+        print(self.target_jpeg_dir + filename + ".jpg")
+        print(self.target_label_dir + filename + ".xml")
+        if os.path.isfile(self.target_jpeg_dir + filename + ".jpg") and os.path.isfile(self.target_label_dir + filename + ".xml"):
+            os.remove(self.target_jpeg_dir + filename + ".jpg")
+            os.remove(self.target_label_dir + filename + ".xml")
+            print(filename + ".jpg, .xml deleted.")
+        else:
+            print(filename + " not found")
 
         self.label_idx += 1
         self.current_idx += 1
@@ -117,9 +152,7 @@ class Application(tk.Frame):
             #plt.close()
 
             self.img1 = Image.open(self.filelist[self.current_idx])
-            width, height = self.img1.size
-            if ( width > 1920 ) or ( height > 1080 ):
-                self.img1 = self.img1.resize(((int)(width/3), (int)(height/3)), Image.ANTIALIAS)
+            self.img1 = self.img1.resize((1024, 768), Image.ANTIALIAS)
 
             #print(self.filelist[self.current_idx], self.img1)
             self.render1 = ImageTk.PhotoImage(self.img1)
@@ -127,11 +160,13 @@ class Application(tk.Frame):
             self.image1.image = self.render1
             self.image1.update()
 
-            #self.img2 = Image.open("test.png")
-            #self.render2 = ImageTk.PhotoImage(self.img2)
-            #self.image2.configure(image=self.render2)
-            #self.image2.image = self.render2
-            #self.image2.update()
+            filename = self.filelist[self.current_idx].split('/')[-1][:-4]
+            self.img2 = Image.open(self.target_jpeg_dir + filename + '.jpg')
+            self.img2 = self.img2.resize((1024, 768), Image.ANTIALIAS)
+            self.render2 = ImageTk.PhotoImage(self.img2)
+            self.image2.configure(image=self.render2)
+            self.image2.image = self.render2
+            self.image2.update()
 
     def update_label(self):
         filename = self.filelist[self.current_idx].split('/')[-1][:-4]
